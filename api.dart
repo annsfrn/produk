cc7e3ddaf46cbf75e5d8ea68c80386582cb19bb2
annsfrn <151833915+annsfrn@users.noreply.github.com>
2024-12-11 12:49:22 +0700

    Add files via upload

---
+++ api.dart
@@ -0,0 +1,7 @@
+class BaseUrl {
+  static String tambah = "http://192.168.137.32/produk/insert.php";
+  static String data = "http://192.168.137.32/produk/list.php";
+  // static String lihat = "http://192.168.137.32/produk/details.php";
+  static String edit = "http://192.168.137.32/produk/update.php";
+  static String hapus = "http://192.168.137.32/produk/delete.php";
+}
